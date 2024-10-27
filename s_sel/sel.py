08e4db35295d547e5b5c6443bd17d7ca356bb482
konst <konstantin.birmenko@gmail.com>
2024-10-27 15:23:33 +0300

    Katya

---
+++ s_sel/sel.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Цикл по всем файлам
+for i in range(306):
+    # Чтение данных из файла
+    data = np.loadtxt(f'array_step_{i}.txt')
+
+    # Создание изображения
+    plt.figure(figsize=(8, 6))
+    plt.plot(data)
+    plt.title(f'Step {i}')
+    plt.savefig(f'step_{i}.png')
+    plt.close()
---
+++ s_shell/shell.py
@@ -0,0 +1,15 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Цикл по всем файлам
+for i in range(503):
+    # Чтение данных из файла
+    data = np.loadtxt(f'iteration_{i}.txt')
+
+    # Создание изображения
+    plt.figure(figsize=(8, 6))
+    plt.plot(data)
+    plt.title(f'Step {i}')
+    plt.savefig(f'step_{i}.png')
+    plt.close()
